0cf7ad9af60c79ab687e4aef1dcc49fc37a82dcd
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2025-07-12 18:20:42 +0530

    phone added

--- src/my components/Home.js
+++ src/my components/Home.js
@@ -2,6 +2,12 @@ import React from 'react'
 import{Link}from'react-router-dom'
 
 export default function Home() {
+const phoneNumber = '+91 7428147159';
+const handleCopy = () => {
+    navigator.clipboard.writeText(phoneNumber)
+    .then(() => alert('Phone number copied! 📞'))
+    .catch(err => alert('Failed to copy number 😵'));
+};
   return (
     < div  id='home' className='section Home'>
         <div className='MainHomeContainer'>
@@ -24,11 +30,11 @@ export default function Home() {
                         <a target="_blank" href="https://www.instagram.com/cheenamakeover" className="social"><img src={require('./Images/instagram.png')} alt="Instragram"/></a>
                         <a target="_blank" href="#" className="social"><img src={require('./Images/facebook.png')} alt="Facebook"/></a>
                         <a target="_blank" href="https://wa.me/7428147159?text=I'm%20interested%20in%20your%20car%20for%20sale" className="social"><img src={require('./Images/whatsapp.png')} alt="WhatsApp"/></a>
-                        <a target="_blank" href="" className="social"><img src={require('./Images/mobile.png')} alt="Phone"/></a>
+                        <a onClick={handleCopy} href="" className="social"><img src={require('./Images/mobile.png')} alt="Phone"/></a>
                     </div>
                 </div>
             </div>
-            <Link className='LINK' to='/bridal'>
+            <Link className='LINK' to='/editorial'>
                 <div className='HomeServiceContainer'>
                     <img className='HomeImage' src={require('./Images/editorial.png')} alt='EDITORIAL'/>
                     <div className='HomeServiceText'>EDITORIAL</div>
