236027d44686575102ad05e36ef29b4b1f40b22e
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2025-07-12 19:06:49 +0530

    switches to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import{BrowserRouter,Routes,Route}from'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './my components/Header';
 import Home from './my components/Home';
 import About from './my components/About';
@@ -10,20 +11,19 @@ import Bridal from './my components/Bridal';
 import Editorial from './my components/Editorial';
 import Booking from './my components/Booking';
 
-
-
 function App() {
   return (
-    <BrowserRouter>
-      <Header/>
+    <Router>
+      <Header />
       <Routes>
-        <Route exact path='/cheena-makeover' element={<><Home/><About/><Gallery/><Services/></>}/>
-        <Route exact path='/bridal' element={<><Bridal/></>}/>
-        <Route exact path='/editorial' element={<><Editorial/></>}/>
-        <Route exact path='/booking' element={<><Booking/></>}/>
-      </Routes>    
-    </BrowserRouter>
+        <Route exact path='/' element={<><Home /><About /><Gallery /><Services /></>} />
+        <Route exact path='/bridal' element={<><Bridal /></>} />
+        <Route exact path='/editorial' element={<><Editorial /></>} />
+        <Route exact path='/booking' element={<><Booking /></>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
