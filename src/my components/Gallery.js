dd3158d9b8beb76820135e6da6871e10b62876b7
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2025-07-12 13:54:01 +0530

    Carousel updated and generalized

--- src/my components/Gallery.js
+++ src/my components/Gallery.js
@@ -1,43 +1,39 @@
-import React from 'react'
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
+const importAll = (r) => {
+  return r.keys().map(r);
+};
+
 export default function Gallery() {
+  const bridalImages = importAll(require.context('./Images/Gallery/Bridal', false, /\.(png|jpe?g|svg)$/));
+  const editorialImages = importAll(require.context('./Images/Gallery/Editorial', false, /\.(png|jpe?g|svg)$/));
+
   return (
     <div id='gallery' className='section Gallery'>
-        <div className='GalleryMainContainer'>
-            <div className='CarouselContainer'>
-                <Carousel fade  prevIcon={<span className="custom-arrow">‹</span>} nextIcon={<span className="custom-arrow">›</span>}>
-                    <Carousel.Item>
-                        <img className="GalleryImage d-block w-100" src={require('./Images/bridal.png')} alt="Bridal Makeup Slide"/>
-                        <Carousel.Caption><h3>Bridal</h3></Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="GalleryImage d-block w-100" src={require('./Images/editorial.png')} alt="Bridal Makeup Slide"/>
-                        <Carousel.Caption><h3>Bridal</h3></Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="GalleryImage d-block w-100" src={require('./Images/about.png')} alt="Bridal Makeup Slide"/>
-                        <Carousel.Caption><h3>Bridal</h3></Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </div>
-            <div className='CarouselContainer'>
-                <Carousel fade  prevIcon={<span className="custom-arrow">‹</span>} nextIcon={<span className="custom-arrow">›</span>}>
-                    <Carousel.Item>
-                        <img className="GalleryImage d-block w-100" src={require('./Images/bridal.png')} alt="Bridal Makeup Slide"/>
-                        <Carousel.Caption><h3>Editorial</h3></Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="GalleryImage d-block w-100" src={require('./Images/editorial.png')} alt="Bridal Makeup Slide"/>
-                        <Carousel.Caption><h3>Editorial</h3></Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="GalleryImage d-block w-100" src={require('./Images/about.png')} alt="Bridal Makeup Slide"/>
-                        <Carousel.Caption><h3>Editorial</h3></Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </div>
+      <div className='GalleryMainContainer'>
+        <div className='CarouselContainer'>
+          <Carousel fade prevIcon={<span className="custom-arrow">‹</span>} nextIcon={<span className="custom-arrow">›</span>}>
+            {bridalImages.map((imgSrc, index) => (
+              <Carousel.Item key={`bridal-${index}`}>
+                <img className="GalleryImage d-block w-100" src={imgSrc} alt={`Bridal Slide ${index + 1}`} />
+                <Carousel.Caption><h3>Bridal</h3></Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
         </div>
+        <div className='CarouselContainer'>
+          <Carousel fade prevIcon={<span className="custom-arrow">‹</span>} nextIcon={<span className="custom-arrow">›</span>}>
+            {editorialImages.map((imgSrc, index) => (
+              <Carousel.Item key={`editorial-${index}`}>
+                <img className="GalleryImage d-block w-100" src={imgSrc} alt={`Editorial Slide ${index + 1}`} />
+                <Carousel.Caption><h3>Editorial</h3></Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </div>
+
+      </div>
     </div>
-  )
+  );
 }
